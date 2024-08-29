cbdd9d15eb9ae9a02d11864f0ab9966ea7969274
vijayprasanth1234 <vijayprasanth2999@gmail.com>
2024-08-30 00:46:58 +0530

    vv

---
+++ numbermethods.js
@@ -0,0 +1,65 @@
+let x=12.345
+let y=x.toFixed(2)
+console.log(y)
+
+
+
+let a=67.890
+let b=a.toFixed(2)
+console.log(b)
+
+
+
+let d=34.567
+let e=d.toFixed(2)
+console.log(e)
+
+
+let f=12.345
+let g=x.toFixed(2)
+console.log(g)
+
+
+
+let h=67.890
+let i=h.toFixed(2)
+console.log(i)
+
+
+
+let j=34.567
+let k=d.toFixed(2)
+console.log(k)
+
+
+let l=12
+let m=Number.isInteger(l)
+console.log(m)
+
+let n=34.56
+let o=Number.isInteger(n)
+console.log(o)
+
+
+
+
+let s="abc"
+let p=isNaN(o)
+console.log(p)
+
+
+let q=123
+let r=isNaN(q)
+console.log(r)
+
+
+
+
+let u=123.456
+let v=x.toPrecision(4)
+console.log(v)
+
+
+let w=789.012
+let z=a.toPrecision(4)
+console.log(z)
